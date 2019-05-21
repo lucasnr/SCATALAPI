00212f725ccddc8b544bf91b98b2c09494f55e5b
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-05-20 22:33:00 -0300

    Serviço de autenticação salva aluno caso seja o primeiro login

--- src/main/java/br/edu/ifrn/scatalapi/service/AutenticacaoService.java
+++ src/main/java/br/edu/ifrn/scatalapi/service/AutenticacaoService.java
@@ -14,7 +14,7 @@ import br.edu.ifrn.scatalapi.model.dto.Credenciais;
 import br.edu.ifrn.suapi.model.AlunoSUAP;
 
 @Path("/login")
-public class AutenticacaoService implements Service{
+public class AutenticacaoService implements Service {
 
 	@POST
 	@Path("/")
@@ -22,16 +22,26 @@ public class AutenticacaoService implements Service{
 	@Produces(MediaType.APPLICATION_JSON)
 	public Token post(Credenciais credenciais) {
 		Token token = new Token(credenciais);
-		
+
 		if (token.isValido()) {
 			AlunoSUAP alunoSUAP = token.getUsuario(AlunoSUAP.class);
-			Aluno aluno = new Aluno(alunoSUAP);
-			AlunoDAO dao = new DAOFactory().getAlunoDAO();
-			
-			Aluno busca = dao.buscaPorMatricula(aluno.getMatricula());
-			
+			String matricula = alunoSUAP.getMatricula();
+
+			salvaAlunoCasoNaoExista(alunoSUAP, matricula);
 		}
-		
+
 		return token;
+	}
+
+	private boolean salvaAlunoCasoNaoExista(AlunoSUAP alunoSUAP, String matricula) {
+		AlunoDAO dao = new DAOFactory().getAlunoDAO();
+		Aluno aluno = dao.buscaPorMatricula(matricula);
+		boolean usuarioNaoExiste = aluno == null;
+		
+		if (usuarioNaoExiste) {
+			aluno = new Aluno(alunoSUAP);
+			return dao.salvar(aluno);
+		} else
+			return false;
 	}
 }
