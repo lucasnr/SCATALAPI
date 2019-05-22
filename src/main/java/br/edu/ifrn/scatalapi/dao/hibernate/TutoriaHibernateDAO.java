0baeb03199f66af9dc8d65859c4bde9e262d8ab3
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-05-22 14:40:33 -0300

    Validando se existe uma disciplina com esse nome

--- src/main/java/br/edu/ifrn/scatalapi/dao/hibernate/TutoriaHibernateDAO.java
+++ src/main/java/br/edu/ifrn/scatalapi/dao/hibernate/TutoriaHibernateDAO.java
@@ -3,6 +3,7 @@ package br.edu.ifrn.scatalapi.dao.hibernate;
 import br.edu.ifrn.scatalapi.dao.DAOFactory;
 import br.edu.ifrn.scatalapi.dao.DisciplinaDAO;
 import br.edu.ifrn.scatalapi.dao.TutoriaDAO;
+import br.edu.ifrn.scatalapi.model.Disciplina;
 import br.edu.ifrn.scatalapi.model.Tutoria;
 
 public class TutoriaHibernateDAO extends AbstractHibernateDAO<Tutoria> implements TutoriaDAO {
@@ -14,7 +15,12 @@ public class TutoriaHibernateDAO extends AbstractHibernateDAO<Tutoria> implement
 	@Override
 	public Tutoria buscaPorNomeDaDisciplina(String nomeDaDisciplina) {
 		DisciplinaDAO disciplinaDAO = new DAOFactory().getDisciplinaDAO();
-		Tutoria tutoria = disciplinaDAO.buscaPorNome(nomeDaDisciplina).getTutoria();
+		Disciplina disciplina = disciplinaDAO.buscaPorNome(nomeDaDisciplina);
+		if (disciplina == null) {
+			return null;
+		}
+		
+		Tutoria tutoria = disciplina.getTutoria();
 		disciplinaDAO.close();
 		return tutoria;
 	}
