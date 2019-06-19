14782316c08e07c00673855cb461f44af0533385
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-06-19 14:45:26 -0300

    Removendo licença do WSO2

--- src/main/java/br/edu/ifrn/scatalapi/Application.java
+++ src/main/java/br/edu/ifrn/scatalapi/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
     public static void main(String[] args) {
 //        int porta = 9090;
 //        if (args.length > 0) {
