0f562f53852d2b8f19e59482aaaef81e98b2855d
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-06-22 21:37:05 -0300

    Removendo o filtro

--- src/main/java/br/edu/ifrn/scatalapi/Application.java
+++ src/main/java/br/edu/ifrn/scatalapi/Application.java
@@ -2,24 +2,11 @@ package br.edu.ifrn.scatalapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-
-import br.edu.ifrn.scatalapi.filter.AutenticacaoFilter;
 
 @SpringBootApplication
 public class Application {
 	
     public static void main(String[] args) {
     	SpringApplication.run(Application.class, args);
-    }
-  
-    @Bean
-    public FilterRegistrationBean<AutenticacaoFilter> filter(){
-    	FilterRegistrationBean<AutenticacaoFilter> registradorDeFiltro = new FilterRegistrationBean<>();
-    	registradorDeFiltro.setFilter(new AutenticacaoFilter());
-    	registradorDeFiltro.addUrlPatterns("/*");
-    	
-    	return registradorDeFiltro;
     }
 }
