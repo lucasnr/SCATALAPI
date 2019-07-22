912f3cc0f6f760860e4794b31b02ea9eee9c33d1
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-07-22 15:07:18 -0300

    Adicionando anotação ComponentScan

--- src/main/java/br/edu/ifrn/scatalapi/Application.java
+++ src/main/java/br/edu/ifrn/scatalapi/Application.java
@@ -4,8 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
+import br.edu.ifrn.scatalapi.controller.TutoriaController;
+import br.edu.ifrn.scatalapi.interceptor.AutenticacaoInterceptor;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.spi.DocumentationType;
@@ -16,6 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
+@ComponentScan(basePackageClasses = {TutoriaController.class, AutenticacaoInterceptor.class})
 public class Application {
 	
     public static void main(String[] args) {
