a7b17632ea2ca1692698dbc99b99d6841c1d101f
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-07-31 18:29:40 -0300

    Documentando AlunoController

--- src/main/java/br/edu/ifrn/scatalapi/controller/AlunoController.java
+++ src/main/java/br/edu/ifrn/scatalapi/controller/AlunoController.java
@@ -19,38 +19,54 @@ import br.edu.ifrn.scatalapi.exception.AlunoComMatriculaNaoEncontrado;
 import br.edu.ifrn.scatalapi.interceptor.AutenticadoRequired;
 import br.edu.ifrn.scatalapi.model.Aluno;
 import br.edu.ifrn.scatalapi.repository.AlunoRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 
 @RestController
 @RequestMapping(value = "/aluno", produces = MediaType.APPLICATION_JSON_VALUE)
 @AutenticadoRequired
+@ApiResponses(@ApiResponse(code = 401, message = "Você não tem permissão para acessar esse recurso ou não informou o token de Autorização"))
 public class AlunoController {
 
 	@Autowired
 	private AlunoRepository repository;
 
 	@GetMapping
+	@ApiOperation(value = "Busca todos os alunos", response = Page.class)
+	@ApiResponses(value = { @ApiResponse(code = 200, message = "Recupera os alunos com sucesso") })
 	public ResponseEntity<Page<AlunoResponseDTO>> findAll(
 			@PageableDefault(page = 0, size = 10, sort = "registro", direction = Direction.DESC) Pageable paginacao) {
-		
+
 		Page<Aluno> alunos = repository.findAll(paginacao);
-		if(alunos.isEmpty())
+		if (alunos.isEmpty())
 			return ResponseEntity.noContent().build();
-		
+
 		return ResponseEntity.ok().body(alunos.map(AlunoResponseDTO::new));
 	}
 
 	@GetMapping(value = "/{id}")
 	@Cacheable(value = "aluno")
-	public AlunoResponseDTO findById(@PathVariable Integer id) {
+	@ApiOperation(value = "Busca um aluno por seu ID", response = AlunoResponseDTO.class)
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "Recupera o aluno com sucesso"),
+			@ApiResponse(code = 404, message = "Não existe aluno com o ID informado") 
+	})
+	public AlunoResponseDTO findById(@ApiParam(required = true, name = "id", value = "O ID do aluno a ser buscado") @PathVariable Integer id) {
 		Aluno aluno = repository.findById(id).orElseThrow(AlunoComIdNaoEncontradoException::new);
 		return new AlunoResponseDTO(aluno);
 	}
 
 	@GetMapping(value = "/matricula/{matricula}")
 	@Cacheable(value = "alunoByMatricula")
-	public AlunoResponseDTO findByMatricula(@PathVariable String matricula) {
-		Aluno aluno = repository.findByMatricula(matricula)
-				.orElseThrow(AlunoComMatriculaNaoEncontrado::new);
+	@ApiOperation(value = "Busca um aluno por sua matrícula", response = AlunoResponseDTO.class)
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "Recupera o aluno com sucesso"),
+			@ApiResponse(code = 404, message = "Não existe aluno com a matrícula informada") 
+	})
+	public AlunoResponseDTO findByMatricula(@ApiParam(required = true, name = "matricula", value = "A matrícula do aluno a ser buscado") @PathVariable String matricula) {
+		Aluno aluno = repository.findByMatricula(matricula).orElseThrow(AlunoComMatriculaNaoEncontrado::new);
 		return new AlunoResponseDTO(aluno);
 	}
 
