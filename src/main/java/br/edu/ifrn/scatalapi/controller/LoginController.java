9d3a6f04936bb5fb1a703388ddf58a6ec6daf85f
Lucas Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-08-08 15:24:33 -0300

    Desativando Autenticação e criando Controller de login de alunos
    Criando Controller para lidar com os dados do aluno logado e desativando a autenticação atual

---
+++ src/main/java/br/edu/ifrn/scatalapi/controller/LoginController.java
@@ -0,0 +1,85 @@
+package br.edu.ifrn.scatalapi.controller;
+
+import javax.transaction.Transactional;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.ifrn.scatalapi.dto.AlunoResponseDTO;
+import br.edu.ifrn.scatalapi.dto.CredenciaisDTO;
+import br.edu.ifrn.scatalapi.dto.TokenDTO;
+import br.edu.ifrn.scatalapi.model.Aluno;
+import br.edu.ifrn.scatalapi.model.Token;
+import br.edu.ifrn.scatalapi.repository.AlunoRepository;
+import br.edu.ifrn.scatalapi.repository.CursoRepository;
+import br.edu.ifrn.scatalapi.repository.EmailRepository;
+import br.edu.ifrn.suapi.exception.CredenciaisIncorretasException;
+import br.edu.ifrn.suapi.exception.FalhaAoConectarComSUAPException;
+import br.edu.ifrn.suapi.exception.TokenInvalidoException;
+import br.edu.ifrn.suapi.model.AlunoSUAP;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import springfox.documentation.annotations.ApiIgnore;
+
+@RestController
+@RequestMapping(value = "/aluno", produces = MediaType.APPLICATION_JSON_VALUE)
+@Api(tags = {"login"}, produces = MediaType.APPLICATION_JSON_VALUE, description = "Operações com o login dos alunos")
+public class LoginController {
+	
+	@Autowired
+	private AlunoRepository alunoRepository;
+	
+	@Autowired
+	private CursoRepository cursoRepository;
+
+	@Autowired
+	private EmailRepository emailRepository;
+
+	@PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE)
+	@Transactional
+	@ApiOperation(value = "Faz login de um aluno usando suas credenciais com o SUAP e retorna o seu token", response = AlunoResponseDTO.class)
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "Recupera o token do aluno com sucesso"),
+			@ApiResponse(code = 400, message = "As credenciais informadas não são válidas") 
+	})
+	public TokenDTO login(@RequestBody @Valid CredenciaisDTO credenciais) throws FalhaAoConectarComSUAPException, CredenciaisIncorretasException {
+		Token token = new Token(credenciais);
+		saveAlunoIfNotExists(token.asAluno());
+		return new TokenDTO(token.getToken());
+	}
+	
+	@GetMapping("/meus-dados")
+	@ApiOperation(value = "Retorna os dados do aluno a partir do Header 'Token'", response = AlunoResponseDTO.class)
+	@ApiResponses(value = { 
+			@ApiResponse(code = 200, message = "Recupera os dados do aluno com sucesso"),
+			@ApiResponse(code = 400, message = "O token não foi informado ou não é válido") 
+	})
+	public AlunoResponseDTO findByToken(@ApiIgnore @RequestHeader(required = true, value = "Token") String tokenHeader) throws FalhaAoConectarComSUAPException, TokenInvalidoException {
+		Token token = new Token(tokenHeader);
+		return new AlunoResponseDTO(alunoRepository.findByMatricula(token.asAluno().getMatricula()).get());
+	}
+	
+	private void saveAlunoIfNotExists(AlunoSUAP aluno) {
+		boolean existe = alunoRepository.findByMatricula(aluno.getMatricula()).isPresent();
+		if(existe)
+			return;
+		
+		Aluno toSave = new Aluno(aluno);
+		toSave.setCurso(cursoRepository.findByCodigoSUAP(toSave.getCurso().getCodigoSUAP()));
+		Aluno savedAluno = alunoRepository.save(toSave);
+		saveAllEmails(savedAluno);
+	}
+
+	private void saveAllEmails(Aluno aluno) {
+		aluno.getEmails().forEach(emailRepository::save);
+	}
+}
--- src/main/java/br/edu/ifrn/scatalapi/interceptor/AutenticacaoInterceptor.java
+++ src/main/java/br/edu/ifrn/scatalapi/interceptor/AutenticacaoInterceptor.java
@@ -3,12 +3,7 @@ package br.edu.ifrn.scatalapi.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
-
-import br.edu.ifrn.scatalapi.exception.TokenInvalidoException;
-import br.edu.ifrn.scatalapi.exception.TokenNaoInformadoException;
-import br.edu.ifrn.scatalapi.model.Token;
 
 public class AutenticacaoInterceptor implements HandlerInterceptor {
 
@@ -16,27 +11,27 @@ public class AutenticacaoInterceptor implements HandlerInterceptor {
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 
-		HandlerMethod handlerMethod;
-		try {
-			handlerMethod = (HandlerMethod) handler;
-		} catch (Exception e) {
-			return true;
-		}
-		
-		boolean isClasseProtegida = handlerMethod.getMethod().getDeclaringClass().isAnnotationPresent(AutenticadoRequired.class);
-		boolean isMetodoProtegido = handlerMethod.getMethod().isAnnotationPresent(AutenticadoRequired.class);
-
-		if (isClasseProtegida || isMetodoProtegido) {
-			String token = request.getHeader("Authorization");
-
-			if (token == null)
-				throw new TokenNaoInformadoException();
-
-			if (!new Token(token).isValido())
-				throw new TokenInvalidoException();
-
-			return true;
-		}
+//		HandlerMethod handlerMethod;
+//		try {
+//			handlerMethod = (HandlerMethod) handler;
+//		} catch (Exception e) {
+//			return true;
+//		}
+//		
+//		boolean isClasseProtegida = handlerMethod.getMethod().getDeclaringClass().isAnnotationPresent(AutenticadoRequired.class);
+//		boolean isMetodoProtegido = handlerMethod.getMethod().isAnnotationPresent(AutenticadoRequired.class);
+//
+//		if (isClasseProtegida || isMetodoProtegido) {
+//			String token = request.getHeader("Authorization");
+//
+//			if (token == null)
+//				throw new TokenNaoInformadoException();
+//
+//			if (!new Token(token).isValido())
+//				throw new TokenInvalidoException();
+//
+//			return true;
+//		}
 
 		return true;
 	}
