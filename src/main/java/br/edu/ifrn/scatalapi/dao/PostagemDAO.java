96bfa3d4d5a10e0b5f0517a40078cd8340820785
Lucas do Nascimento <lucasnascimentoribeiro13@gmail.com>
2019-08-13 21:47:18 -0300

    Removendo antigos DAOs

--- src/main/java/br/edu/ifrn/scatalapi/dao/PostagemDAO.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifrn.scatalapi.dao;
-
-import br.edu.ifrn.scatalapi.model.Postagem;
-
-public interface PostagemDAO extends AbstractDAO<Postagem>{
-
-	public Postagem buscaPorId(Integer id);
-
-}
